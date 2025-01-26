751169364add8205b00d27240b676e1f7c4fd583
Asmit <asmitkatiyar33@gmail.com>
2025-01-26 18:25:43 +0530

    bug resolve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,188 +1,123 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import "bootstrap/dist/js/bootstrap.bundle.min"; // Import Bootstrap JS
-import AOS from "aos";
+import AOS from "aos"; // Import AOS
 import "aos/dist/aos.css"; // Import AOS styles
 
 function Navbar() {
-  const offcanvasRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const closeOffcanvas = () => {
-    const offcanvasElement = offcanvasRef.current;
-    if (offcanvasElement) {
-      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
-      bsOffcanvas && bsOffcanvas.hide();
-    }
-  };
-
+  // Initialize AOS when the component mounts
   useEffect(() => {
     AOS.init({
       duration: 1000, // Duration of the animation in milliseconds
-      once: true, // Whether animation should happen only once - while scrolling down
+      once: true, // Whether animation should happen only once
     });
   }, []);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+      <nav className="navbar">
+        <div className="navbar-container">
+          <a href="#" className="navbar-logo">
             <img
-              className="rounded-circle logo-image"
+              className="navbar-logo-image"
               src="/img/my-profile-img.jpg"
               alt="Profile Image"
-              loading="lazy"
             />
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasDarkNavbar"
-            aria-controls="offcanvasDarkNavbar"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" onClick={toggleMenu}>
+            &#9776; {/* Hamburger icon */}
           </button>
-          <div
-            className="offcanvas offcanvas-end text-bg-dark"
-            tabIndex="-1"
-            id="offcanvasDarkNavbar"
-            aria-labelledby="offcanvasDarkNavbarLabel"
-            ref={offcanvasRef}
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                My-Profile
-              </h5>
-              <button
-                type="button"
-                className="btn-close btn-close-white"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <Link
-                    to="home"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link"
-                    onClick={closeOffcanvas}
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item" data-aos="fade-up">
-                  <Link
-                    to="about"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link"
-                    onClick={closeOffcanvas}
-                  >
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item" data-aos="fade-left">
-                  <Link
-                    to="resume"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link"
-                    onClick={closeOffcanvas}
-                  >
-                    Resume
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="portfolio"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link pointer"
-                    onClick={closeOffcanvas}
-                  >
-                    Portfolio
-                  </Link>
-                </li>
-                <li className="nav-item" data-aos="fade-up">
-                  <Link
-                    to="services"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link"
-                    onClick={closeOffcanvas}
-                  >
-                    Services
-                  </Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu dropdown-menu-dark">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="contact"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="nav-link"
-                    onClick={closeOffcanvas}
-                  >
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <ul className={`navbar-menu ${isOpen ? "active" : ""}`}>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="navbar-link"
+                activeClass="active"
+                onClick={() => setIsOpen(false)}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                activeClass="active"
+                className="navbar-link"
+                onClick={() => setIsOpen(false)}
+              >
+                About
+              </Link>
+            </li>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="resume"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                activeClass="active"
+                className="navbar-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Resume
+              </Link>
+            </li>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="portfolio"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                activeClass="active"
+                className="navbar-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="services"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                activeClass="active"
+                className="navbar-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="navbar-item" data-aos="fade-up">
+              <Link
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                activeClass="active"
+                className="navbar-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
